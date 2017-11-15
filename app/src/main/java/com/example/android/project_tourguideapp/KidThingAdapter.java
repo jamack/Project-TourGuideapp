45b49a9b654a9b526b2ceeed60f6d5096de8a5a5
jamack <jamack9@hotmail.com>
2017-11-15 13:06:59 -0600

    Implemented ViewHolder pattern for KidThingAdapter. (Per review comments).

--- app/src/main/java/com/example/android/project_tourguideapp/KidThingAdapter.java
+++ app/src/main/java/com/example/android/project_tourguideapp/KidThingAdapter.java
@@ -38,10 +38,37 @@ class KidThingAdapter extends ArrayAdapter<KidThing> {
         //Get the {@link KidThing} object located at this position in the list
         KidThing currentKidThing = getItem(position);
 
+        // Cache with view lookup for a KidThing object which will be stored in a tag
+        ViewHolder viewHolder;
+
         // Check if the current view is being reused or needs to be inflated
         View listItemView = convertView;
         if (listItemView == null) {
+            // Instantiate new ViewHolder object to cache Views for current KidThing
+            viewHolder = new ViewHolder();
+
+            // Inflate current view
             listItemView = LayoutInflater.from(getContext()).inflate(R.layout.list_item, parent, false);
+
+            // Perform View lookups and cache those in the ViewHolder:
+
+            // Find the TextView in the list_item layout with the ID listing_title
+            viewHolder.listingTitle = (TextView) listItemView.findViewById(R.id.listing_title);
+
+            // Find the ImageView in the list_item layout with the ID listing_image
+            viewHolder.listingImage = (ImageView) listItemView.findViewById(R.id.listing_image);
+
+            // Find the TextView in the list_item layout with the ID listing_hours_dates
+            viewHolder.listingHoursDates = (TextView) listItemView.findViewById(R.id.listing_hours_dates);
+
+            // Find the TextView in the list_item layout with the ID listing_description_snippet
+            viewHolder.listingDescription = (TextView) listItemView.findViewById(R.id.listing_description_snippet);
+
+            // Cache the viewHolder object inside the fresh view
+            listItemView.setTag(viewHolder);
+
+        } else { // View is being recycled, retrieve the viewHolder object from tag
+            viewHolder = (ViewHolder) convertView.getTag();
         }
 
         // Set the background color, alternating colors based upon items position in the arraylist
@@ -51,40 +78,42 @@ class KidThingAdapter extends ArrayAdapter<KidThing> {
             listItemView.setBackgroundColor(Color.parseColor("#" + Integer.toHexString(ContextCompat.getColor(getContext(), R.color.colorAccentLight))));
         }
 
-        // Find the TextView in the list_item layout with the ID listing_title
-        TextView listingTitle = (TextView) listItemView.findViewById(R.id.listing_title);
-        listingTitle.setText(currentKidThing.getListingName());
+        // Set the listing name on the appropriate TextView cached in the ViewHolder
+        viewHolder.listingTitle.setText(currentKidThing.getListingName());
 
         // Check whether the current item has an associated thumbnail image. (Banner image isn't used here).
-        // If so, find the ImageView in the list_item layout with the ID listing listing_image
-        // & assign image to the ImageView.
+        // If so, assign image to the ImageView cached in the ViewHolder.
         // If no associated image, turn the ImageView off.
-        ImageView listingImage = (ImageView) listItemView.findViewById(R.id.listing_image);
         if (currentKidThing.hasImage()) {
-            listingImage.setImageResource(currentKidThing.getThumbnailImageResourceId());
-            listingImage.setVisibility(View.VISIBLE);
+            viewHolder.listingImage.setImageResource(currentKidThing.getThumbnailImageResourceId());
+            viewHolder.listingImage.setVisibility(View.VISIBLE);
         } else {
-            listingImage.setVisibility(View.GONE);
+            viewHolder.listingImage.setVisibility(View.GONE);
         }
 
         // Check whether the current item has a associated hours/dates.
-        // Find the TextView in the list_item layout with the ID listing_hours_dates
-        // & assign text to the TextView.
+        // If so, assign text to the TextView cached in the ViewHolder.
         // If no associated hours/dates, turn the TextView off.
-        TextView listingHoursDates = (TextView) listItemView.findViewById(R.id.listing_hours_dates);
         if (currentKidThing.hasHoursDates()) {
-            listingHoursDates.setText(currentKidThing.getHoursDates());
-            listingHoursDates.setVisibility(View.VISIBLE);
+            viewHolder.listingHoursDates.setText(currentKidThing.getHoursDates());
+            viewHolder.listingHoursDates.setVisibility(View.VISIBLE);
         } else {
-            listingHoursDates.setVisibility(View.GONE);
+            viewHolder.listingHoursDates.setVisibility(View.GONE);
         }
 
-        // Find the TextView in the list_item layout with the ID listing_description_snippet
-        TextView listingDescription = (TextView) listItemView.findViewById(R.id.listing_description_snippet);
-        listingDescription.setText(currentKidThing.getDescription());
+        // Set the listing description snippet on the appropriate TextView cached in the ViewHolder
+        viewHolder.listingDescription.setText(currentKidThing.getDescription());
 
         // Return the whole list item layout (containing all Views) so that it can be shown in the ListView
         return listItemView;
 
+    }
+
+    // View lookup cache
+    private static class ViewHolder {
+        TextView listingTitle;
+        ImageView listingImage;
+        TextView listingHoursDates;
+        TextView listingDescription;
     }
 }
